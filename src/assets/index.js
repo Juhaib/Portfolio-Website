6188ba51bab27a976a079d13a85c9bd4c1404de4
Juhaib <juhaibkhanbca2k13@gmail.com>
2024-11-21 00:48:32 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,94 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import excel from "./company/excel.png";
+import lead from "./company/lead.png";
+import functionup from "./company/functionup.png";
+import data from "./tech/data.png";
+import tensorflow from "./tech/tensorflow.png";
+import keras from "./tech/keras.png";
+import sckit from "./tech/sckit.png";
+import jupyter from "./tech/jupyter.png";
+import ai from "./tech/ai.png";
+import machine from "./tech/machine.png";
+import sql from "./tech/sql.png";
+import llm from "./tech/llm.png";
+import time from "./tech/time.png";
+import rag from "./tech/rag.png";
+import snowflake from "./tech/snowflake.png";
+import prob from "./tech/prob.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  excel,
+  lead,
+  functionup,
+  data,
+  tensorflow,
+  keras,
+  sckit,
+  jupyter,
+  ai,
+  machine,
+  sql,
+  llm,
+  time,
+  rag,
+  snowflake,
+  prob
+};
---
+++ src/constants/index.js
@@ -0,0 +1,293 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  excel,
+  lead,
+  functionup,
+  data,
+  tensorflow,
+  keras,
+  sckit,
+  jupyter,
+  ai,
+  machine,
+  sql,
+  llm,
+  time,
+  rag,
+  snowflake,
+  prob
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Data Scientist",
+    icon: web,
+  },
+  {
+    title: "Python Developer",
+    icon: mobile,
+  },
+  {
+    title: "Machine Learning",
+    icon: backend,
+  },
+  {
+    title: "Artificial Intelligence",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "Data Scientist",
+    icon: data,
+  },
+  {
+    name: "Machine Learning",
+    icon: machine,
+  },
+  {
+    name: "Artificial Intelligence",
+    icon: ai,
+  },
+  {
+    name: "Jupyter",
+    icon: jupyter,
+  },
+  {
+    name: "TesorFlow",
+    icon: tensorflow,
+  },
+  {
+    name: "Keras",
+    icon: keras,
+  },
+  {
+    name: "Scikit Learn",
+    icon: sckit,
+  },
+  {
+    name: "SQL",
+    icon: sql,
+  },
+  {
+    name: "LLM",
+    icon: llm,
+  },
+  {
+    name: "RAG",
+    icon: rag,
+  },
+  {
+    name: "Time Series Forecasting",
+    icon: time,
+  },
+  {
+    name: "Probability",
+    icon: prob,
+  },
+
+  {
+    name: "Snowflake",
+    icon: snowflake,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Data Scientist Trainee",
+    company_name: "ExcelR",
+    icon: excel,
+    iconBg: "#E6DEDD",
+    date: "May 2024 - Present",
+    points: [
+      "Developed data-driven insights that boosted strategic decisionmaking, increasing operational efficiency by 15%",
+      "Implemented a data cleaning process that reduced errors by 20%, ensuring higher analysis accuracy",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Collaborated with marketing, finance, and product teams to deliver analytical solutions, contributing to a 10% business growth.",
+    ],
+  },
+  {
+    title: "Data Scientist Intern",
+    company_name: "Lead Height",
+    icon: lead,
+    iconBg: "#383E56",
+    date: "Feb 2024 - Aug 2024",
+    points: [
+      "Collected, analyzed, and modeled data using statistical techniques and machine learning algorithms",
+      "Developed interactive dashboards and reports to communicate insights, optimizing processes and achieving cost savings",
+      "Worked with cross-functional teams to identify process improvement opportunities and implemented data-driven solutions",
+      "Optimized inventory management, reduced equipment downtime with predictive maintenance models, and received recognition for outstanding performance",
+    ],
+  },
+  {
+    title: "Full Stack Trainee",
+    company_name: "FunctionUP",
+    icon: functionup,
+    iconBg: "#E6DEDD",
+    date: "Mar 2022 - Oct 2022",
+    points: [
+      "Create and manage the marketing budget, ensuring efficient allocation of resources and optimizing ROI.",
+      "Oversee market research to identify emerging trends, customer needs, and competitor strategies.",
+      "Monitor brand consistency across marketing channels and materials",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Juhaib proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Juhaib does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Juhaib optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Random Forest",
+    description:
+      "I implemented a Random Forest model for classifying the Glass dataset. The project involved data preprocessing, feature scaling, and evaluating the model in Jupyter Notebook, comparing its performance with Bagging and Boosting methods.",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "eda",
+        color: "green-text-gradient",
+      },
+      {
+        name: "machine-learning",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/Juhaib/Random-Forest",
+  },
+  {
+    name: "Clustering",
+    description:
+      "Performed clustering with K-Means, Hierarchical Clustering, and DBSCAN after preprocessing and normalizing features. Evaluated clusters using the silhouette score and visualized results with scatter plots and dendrograms.",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "K-Means",
+        color: "green-text-gradient",
+      },
+      {
+        name: "DBSCAN",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/Juhaib/Clustering",
+  },
+  {
+    name: "Logistics Regression",
+    description:
+      "Applied Logistic Regression for binary classification with data preprocessing and feature scaling. Evaluated the model using accuracy, precision, recall, and ROC-AUC, determining its overall predictive performance.",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "visualization",
+        color: "green-text-gradient",
+      },
+      {
+        name: "machine-learning",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/Juhaib/Logistics_Regression",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
