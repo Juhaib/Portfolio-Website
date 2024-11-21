393f4cef21df7e64a4fa98d7c44c804a3c15d59c
Juhaib Khan <72125559+Juhaib@users.noreply.github.com>
2024-11-21 18:47:56 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -77,6 +77,59 @@ const services = [
 
 const technologies = [
   {
+    name: "Data Scientist",
+    icon: data,
+  },
+  {
+    name: "Machine Learning",
+    icon: machine,
+  },
+  {
+    name: "Artificial Intelligence",
+    icon: ai,
+  },
+  {
+    name: "Jupyter",
+    icon: jupyter,
+  },
+  {
+    name: "tesorFlow",
+    icon: tensorflow,
+  },
+  {
+    name: "keras",
+    icon: keras,
+  },
+  {
+    name: "scikit Learn",
+    icon: sckit,
+  },
+  {
+    name: "SQL",
+    icon: sql,
+  },
+  {
+    name: "LLM",
+    icon: llm,
+  },
+  {
+    name: "RAG",
+    icon: rag,
+  },
+  {
+    name: "Time Series Forecasting",
+    icon: time,
+  },
+  {
+    name: "Probability",
+    icon: prob,
+  },
+
+  {
+    name: "snowflake",
+    icon: snowflake,
+  },
+  {
     name: "HTML 5",
     icon: html,
   },
@@ -89,44 +142,16 @@ const technologies = [
     icon: javascript,
   },
   {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
     name: "React JS",
     icon: reactjs,
-  },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
   },
   {
     name: "Node JS",
     icon: nodejs,
   },
   {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
     name: "git",
     icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
   },
 ];
 
